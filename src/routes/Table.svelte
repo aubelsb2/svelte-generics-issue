5838b10f678c697a5dfd508b2fd9c6cff74d0a60
Arran Ubels <a.ubels@base2services.com>
2023-06-13 14:12:49 +1000

    Table defined

--- src/routes/Table.svelte
+++ src/routes/Table.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    export let rows : Array<T>
 
     type T = $$Generic;
     interface $$Slots {
         row: {
             row: T;
         }
-
     }
 </script>
+
+<ul>
+    {#each row as rows}
+        <slot name="row" {row}>
+            <li>???</li>
+        </slot>
+    {/each}
+</ul>
